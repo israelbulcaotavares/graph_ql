b4e78dedbdc31aa77e8b4f261bc3bf0267cec7c2
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-02 12:37:48 -0300

    Organizando Código #01

--- 01-schema-query/index.js
+++ 01-schema-query/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { importSchema } = require('graphql-import')
 
 const perfis =[
     {id:1 ,nome: 'comum'},
@@ -24,50 +25,7 @@ const usuarios =[{
     email: 'danismi@umail.com',
     idade:24,
     perfil_id: 1
-}]
-
-const typeDefs = gql`
-
-    scalar Date
-
-    
-    type Produto{
-        nome: String!
-        preco: Float!
-        desconto: Float
-        precoComDesconto: Float
-        
-    }
-
-    type Perfil{
-        id: Int
-        nome: String
-    }
-    
-    type Usuario {
-        id: Int
-        nome: String!
-        email: String!
-        idade: Int 
-        salario: Float
-        vip: Boolean
-        perfil: Perfil
-    }
-
-    # Pontos de entrada da sua API!
-    type Query { 
-        ola: String!
-        horaAtual: Date! 
-        usuarioLogado: Usuario
-        produtoEmDestaque: Produto
-        numerosMegaSena: [Int!]!
-        usuarios: [Usuario]
-        usuario(id: Int): Usuario 
-        perfis: [Perfil]
-        perfil(id: Int): Perfil
-
-    }
-`
+}];
 
 // RESOLVERS para relacionar Tipos
 const resolvers = {
@@ -149,7 +107,7 @@ const resolvers = {
 }
 
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: importSchema('./schema/index.graphql'),
     resolvers
 })
 
