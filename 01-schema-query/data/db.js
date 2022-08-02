18537857242ac42296d088526273e93788e25dc0
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-02 15:03:52 -0300

    Enum

--- 01-schema-query/data/db.js
+++ 01-schema-query/data/db.js
@@ -9,19 +9,22 @@ const usuarios =[{
     nome: 'Joao Silva',
     email : 'jsilva@zemail.com',
     idade: 29,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'ATIVO'
 },{
     id: 2,
     nome:'Rafael Junior',
     email: 'rafajun@wgmail.com',
     idade:31,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'INATIVO'
 },{
     id: 3,
     nome:'Daniela Smith',
     email: 'danismi@umail.com',
     idade:24,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'BLOQUEADO'
 }];
 
 module.exports = { usuarios, perfis }
